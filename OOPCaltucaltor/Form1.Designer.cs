fe383b02848f571e41919788c504dd9e12a88044
YuriTrue1 <90495431+YuriTrue1@users.noreply.github.com>
2021-12-23 13:30:13 +0300

    Add files via upload

---
+++ OOPCaltucaltor/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 50);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBox2.Location = new System.Drawing.Point(12, 84);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(50, 50);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBox3.Location = new System.Drawing.Point(178, 28);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(50, 50);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBox4.Location = new System.Drawing.Point(178, 84);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(50, 50);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.Leave += new System.EventHandler(this.textBox4_Leave);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(78, 70);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(91, 32);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "+";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button1.Location = new System.Drawing.Point(84, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "=";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Info;
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label1.Location = new System.Drawing.Point(87, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 52);
+            this.label1.TabIndex = 8;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 327);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OOPCaltucaltor/Rational.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Rational
+    {
+        int num, den;
+        public Rational(int num, int den)
+        {
+            this.num = num;
+            this.den = den;
+        }
+        public static Rational operator +(Rational rational1, Rational rational2)
+        {
+            if (rational1.den == 0 || rational2.den == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            int numResult;
+            int denResult;
+            Rational rational;
+            numResult = rational1.num * rational2.den + rational2.num * rational1.den;
+            denResult = rational1.den * rational2.den;
+            rational = new Rational(numResult, denResult);
+            return rational;
+        }
+        public static Rational operator -(Rational rational1, Rational rational2)
+        {
+            if (rational1.den == 0 || rational2.den == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            int numResult;
+            int denResult;
+            Rational rational;
+            numResult = rational1.num * rational2.den - rational2.num * rational1.den;
+            denResult = rational1.den * rational2.den;
+            rational = new Rational(numResult, denResult);
+            return rational;
+        }
+        public static Rational operator *(Rational rational1, Rational rational2)
+        {
+            if (rational1.den == 0 || rational2.den == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            int numResult;
+            int denResult;
+            Rational rational;
+            numResult = rational1.num * rational2.num;
+            denResult = rational1.den * rational2.den;
+            rational = new Rational(numResult, denResult);
+            return rational;
+        }
+        public static Rational operator /(Rational rational1, Rational rational2)
+        {
+            if (rational1.den == 0 || rational2.den == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            int numResult = rational1.num * rational2.den;
+            int denResult = rational1.den * rational2.num;
+            if (numResult == Math.Abs(0) || denResult == Math.Abs(0))
+            {
+                throw new DivideByZeroException();
+            }
+            Rational rational = new Rational(numResult, denResult);
+            return rational;
+        }
+        public override string ToString()
+        {
+            string result = "";
+            if (this.num != this.den)
+            {
+                result = $"{this.num} / {this.den}";
+            }
+            if (this.num == Math.Abs(0))
+            {
+                result = $"{0}";
+            }
+            if (this.den == 1)
+            {
+                result = $"{this.num}";
+            }
+            return result;
+        }
+        public Rational Reduction()
+        {
+            int numResult = Math.Abs(this.num);
+            int denResult = Math.Abs(this.den);
+            if (numResult != denResult && numResult != 0)
+            {
+                while (numResult != denResult)
+                {
+                    if (numResult > denResult)
+                    {
+                        numResult = numResult - denResult;
+                    }
+                    else
+                    {
+                        denResult = denResult - numResult;
+                    }
+                }
+                int nod = numResult;
+                numResult = this.num / nod;
+                denResult = this.den / nod;
+            }
+            else if (numResult == denResult)
+            {
+                numResult = numResult / numResult;
+                denResult = denResult / denResult;
+            }
+            Rational result = new Rational(numResult, denResult);
+            return result;
+        }
+    }
+}
